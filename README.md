74c8bc96118b051c7a27dff2774816a2b23bbf56
robmllze <86869208+robmllze@users.noreply.github.com>
2024-11-20 16:11:51 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NeoVim Extension
+
+## nvim-ex-color-line-numbers-by-mode
+
+An extension for NeoVim to color the line numbers baded on the current mode to help you better see the mode you're in.
---
+++ init.lua
@@ -0,0 +1,35 @@
+-- Copyright (c) 2023, Robert Mollentze, DevCetra.com
+
+local M = {}
+
+local mode_colors = {
+    n = "#44a8e5",    -- Normal
+    i = "#c177e5",    -- Insert
+    v = "#88deff",    -- Visual
+    c = "#afcfa4",    -- Command
+    t = "#afcfa5",    -- Terminal
+    default = "#FFFFFF" -- Fallback
+}
+local function color_line_numbers_by_node()
+    local mode = vim.fn.mode()
+    local color = mode_colors[mode] or mode_colors.default
+    vim.cmd(string.format("highlight LineNr guifg=%s", color)) -- Fixed highlight group
+    vim.wo.number = true
+    vim.wo.cursorline = mode ~= "default"
+    vim.wo.cursorlineopt = mode ~= "default" and "number" or "line"
+    vim.wo.signcolumn = mode ~= "default" and "yes" or "auto"
+end
+
+-- Setup function.
+function M.setup()
+    vim.api.nvim_create_autocmd("ModeChanged", {
+        pattern = "*",
+        callback = function()
+            print("Mode changed: " .. vim.fn.mode()) 
+            color_line_numbers_by_node()
+        end,
+    })
+    color_line_numbers_by_node()
+end
+
+return M
